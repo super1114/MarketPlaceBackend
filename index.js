3150e2f15f7333dea1ae1e85d29906533ee03932
super1114 <super1114dev@gmail.com>
2023-03-09 13:48:14 +0900

    init project

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
 const express = require('express');
-const multer = require('multer');
-const mongoose = require('mongoose');
-const bodyparser = require('body-parser');
-const cors = require('cors')
+// const multer = require('multer');
+// const mongoose = require('mongoose');
+// const bodyparser = require('body-parser');
+// const cors = require('cors')
 
 const PORT = 8000;
-const MONGODB_URI = "mongodb+srv://vercel-admin-user-64078ccf3f79391698b130d1:ApH30bpK6LZW1QJZ@xrpl-cluster.j3xqx9d.mongodb.net/nftDB?retryWrites=true&w=majority"
+// const MONGODB_URI = "mongodb+srv://vercel-admin-user-64078ccf3f79391698b130d1:ApH30bpK6LZW1QJZ@xrpl-cluster.j3xqx9d.mongodb.net/nftDB?retryWrites=true&w=majority"
 const app = express();
-const upload = multer({ dest: 'public/' });
+// const upload = multer({ dest: 'public/' });
 
 //use static files
 app.use(express.static('public'));
 //middleware
-app.use(bodyparser.urlencoded({ extended: true }));
-app.use(bodyparser.json());
-app.use(cors());
-app.use(express.json());
+// app.use(bodyparser.urlencoded({ extended: true }));
+// app.use(bodyparser.json());
+// app.use(cors());
+// app.use(express.json());
 
 //Router
 app.get("/", (req, res) => {
   res.send("Hello James");
 });
 
-app.post("/create", upload.single('image'), async (req, res) => {
-  let imageName = req.file.filename;
-  const imagePath = `localhost:3000/${imageName}`;
-  mongoose.connect(MONGODB_URI);
-  const db = mongoose.connection;
-  db.on("error", (error) => {
-    res.send(error);
-  });
-  db.once("open", async () => {
+// app.post("/create", upload.single('image'), async (req, res) => {
+//   let imageName = req.file.filename;
+//   const imagePath = `localhost:3000/${imageName}`;
+//   mongoose.connect(MONGODB_URI);
+//   const db = mongoose.connection;
+//   db.on("error", (error) => {
+//     res.send(error);
+//   });
+//   db.once("open", async () => {
 
-    let newNFT = {
-      nftImgPath: imagePath,
-      nftName: req.body.nftName,
-      nftDescription: req.body.nftDescription,
-      nftProperties: req.body.nftDescription
-    };
+//     let newNFT = {
+//       nftImgPath: imagePath,
+//       nftName: req.body.nftName,
+//       nftDescription: req.body.nftDescription,
+//       nftProperties: req.body.nftDescription
+//     };
 
-    let nft = new NFTModel(newNFT);
-    try {
-      await nft.save();
-      res.send("NFT info saved");
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  })
-})
+//     let nft = new NFTModel(newNFT);
+//     try {
+//       await nft.save();
+//       res.send("NFT info saved");
+//     } catch (error) {
+//       res.status(500).send(error);
+//     }
+//   })
+// })
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
