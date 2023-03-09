0a99bc60cbc070c5a05f4becf722e148bca5c53c
super1114 <super1114dev@gmail.com>
2023-03-09 13:53:06 +0900

    insert create feature

--- model/model.js
+++ model/model.js
@@ -1,24 +1,24 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const NFTSchema = new mongoose.Schema({
-//     nftImgPath: {
-//         type: String,
-//         require: true
-//     },
-//     nftName: {
-//         type: String,
-//         required: true,
-//     },
-//     nftDescription: {
-//         type: String,
-//         required: true,
-//     },
-//     nftProperties: {
-//         type: Object,
-//         required: true,
-//     },
-// });
+const NFTSchema = new mongoose.Schema({
+    nftImgPath: {
+        type: String,
+        require: true
+    },
+    nftName: {
+        type: String,
+        required: true,
+    },
+    nftDescription: {
+        type: String,
+        required: true,
+    },
+    nftProperties: {
+        type: Object,
+        required: true,
+    },
+});
 
-// const NFTModel = mongoose.model("NFTS", NFTSchema);
+const NFTModel = mongoose.model("NFTS", NFTSchema);
 
-// module.exports = NFTModel;
+module.exports = NFTModel;
